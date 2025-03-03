98914834239364779bb6305d37895428841e0281
creynolds8 <cameron8.cr@gmail.com>
2025-03-03 10:12:37 -0800

    tutorial: complete Expo tutorial add navigation section

---
+++ app/+not-found.tsx
@@ -0,0 +1,29 @@
+import { View, StyleSheet } from 'react-native';
+import { Link, Stack } from 'expo-router';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Ooops! Not Found'}} />
+      <View>
+        <Link href='/' style={styles.button}>
+          Go back to Home screen
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
