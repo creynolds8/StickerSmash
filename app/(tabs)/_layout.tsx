a33345b5ddde47db61cd4a52a5a18a8d11d5d049
creynolds8 <cameron8.cr@gmail.com>
2025-03-04 14:26:13 -0800

    tutorial: complete Expo tutorial ch. 9

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,33 +16,31 @@ export default function TabLayout() {
         },
       }}
     >
-      <Tabs>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Home",
-            tabBarIcon: ({ color, focused }) => (
-              <Ionicons
-                name={focused ? 'home-sharp' : 'home-outline'}
-                color={color}
-                size={24} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="about"
-          options={{
-            title: "About",
-            tabBarIcon: ({ color, focused }) => (
-              <Ionicons
-                name={focused ? 'information-circle' : 'information-circle-outline'}
-                color={color}
-                size={24}
-              />
-            ),
-          }}
-        />
-      </Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'home-sharp' : 'home-outline'}
+              color={color}
+              size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "About",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'information-circle' : 'information-circle-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
